97b12e7f852c6160868bba1e5f62f381681af872
Rebecca333 <rebeccawhite@verizon.net>
2017-02-25 10:38:55 -0500

    Create 99BottlesV1.py

---
+++ 99BottlesV1.py
@@ -0,0 +1,3 @@
+for number in range(98):
+	print "{0} bottles of beer on the wall, {0} bottles of beer . . . ".format(99-number)
+	print "If one of those bottles should happen to fall, {0} bottles of beer on the wall".format(98-number)
